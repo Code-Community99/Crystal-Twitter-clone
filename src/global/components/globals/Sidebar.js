69a071ea2d5b7952fd40b8262907d67dfcc0d9f4
dalmasonto <dalmasogembo@gmail.com>
2021-04-06 05:03:13 +0300

    setting up notifications page

--- src/global/components/globals/Sidebar.js
+++ src/global/components/globals/Sidebar.js
@@ -3,7 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import { moreLinks, routes } from '../../constants/Constants';
 import './../../styles/Sidebar.css';
 import { FaCheck, FaDotCircle, FaEllipsisH, FaTwitter } from 'react-icons/fa';
-import { IoEllipsisHorizontal } from 'react-icons/io5';
+import { IoEllipsisHorizontal, IoEllipsisHorizontalCircleOutline } from 'react-icons/io5';
 
 import { Avatar, Button, IconButton } from '@material-ui/core'
 import { Popover } from 'uiw'
@@ -19,19 +19,21 @@ function Sidebar() {
   return (
     <div className="sidebar">
 
-      <div className="main-sidebar">
+      <div className="sidebar-sticky h-100">
+        <div className="position-relative h-100">
+          <div className="main-sidebar">
 
-        <div className="twitter-logo">
-          <IconButton className="twitter-btn" onClick={e => history.push('/')}>
-            <FaTwitter />
-          </IconButton>
-        </div>
+            <div className="twitter-logo">
+              <IconButton className="twitter-btn" onClick={e => history.push('/')}>
+                <FaTwitter />
+              </IconButton>
+            </div>
 
-        {/* This are the visible links */}
-        <MenuLinks />
+            {/* This are the visible links */}
+            <MenuLinks />
 
-        {/* UNCOMMENT/COMMENT THIS SECTION FOR A POPOVER MENU */}
-        {/* <Popover trigger="click" content={<div className="more-menu-popover"><MoreLinks /></div>}>
+            {/* UNCOMMENT/COMMENT THIS SECTION FOR A POPOVER MENU */}
+            {/* <Popover trigger="click" content={<div className="more-menu-popover"><MoreLinks /></div>}>
           <div className="sidebar-link more-link">
             <span className="sidebar-link-icon">
               {<IoEllipsisHorizontal />}
@@ -42,47 +44,48 @@ function Sidebar() {
           </div>
         </Popover> */}
 
-        {/* POPOVER NORMAL MENU */}
-        {/* You can comment this out to use th above menu instead */}
-        <div className="sidebar-link more-link" onClick={e => setMoreActive(prev => !prev)}>
-          <span className="sidebar-link-icon">
-            {<IoEllipsisHorizontal />}
-          </span>
-          <span className="sidebar-link-title">
-            More
-          </span>
-        </div>
-        <div className={`more-menu ${moreActive ? 'more-active' : ''}`}>
-          <IconButton className="menu-closer" onClick={e => setMoreActive(prev => !prev)}>
-            <IoIosClose />
-          </IconButton>
-          <MoreLinks closeFunc={e => setMoreActive(prev => !prev)} />
-        </div>
-        {/* POPOVER NORMAL MENU */}
+            {/* POPOVER NORMAL MENU */}
+            {/* You can comment this out to use th above menu instead */}
+            <div className="sidebar-link more-link" onClick={e => setMoreActive(prev => !prev)}>
+              <span className="sidebar-link-icon">
+                {<IoEllipsisHorizontalCircleOutline size={22} />}
+              </span>
+              <span className="sidebar-link-title">
+                More
+              </span>
+            </div>
+            <div className={`more-menu ${moreActive ? 'more-active' : ''}`}>
+              <IconButton className="menu-closer" onClick={e => setMoreActive(prev => !prev)}>
+                <IoIosClose />
+              </IconButton>
+              <MoreLinks closeFunc={e => setMoreActive(prev => !prev)} />
+            </div>
+            {/* POPOVER NORMAL MENU */}
 
-        {/* TWEET BUTTON */}
-        <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-          <Button className="tweet-button">
-            Tweet
+            {/* TWEET BUTTON */}
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <Button className="tweet-button">
+                Tweet
           </Button>
-        </div>
+            </div>
 
-      </div>
-
-      {/* Bottom side of the sidebar */}
-      <Popover placement='top' trigger='click' content={<MoreAccountOptions />} className="more-account-optionss">
-        <div className="sidebar-bottom">
-          <Avatar src="https://pbs.twimg.com/profile_images/1357950864674205696/8bVQrFvB_400x400.jpg" />
-          <div className="middle-details">
-            <h3>Dalmas Ogembo</h3>
-            <p>@dalmasonto</p>
           </div>
-          <IconButton className="last-icon">
-            <FaEllipsisH />
-          </IconButton>
-        </div>
-      </Popover>
 
+          {/* Bottom side of the sidebar */}
+          <Popover placement='top' trigger='click' content={<MoreAccountOptions />} className="more-account-optionss">
+            <div className="sidebar-bottom">
+              <Avatar src="https://pbs.twimg.com/profile_images/1357950864674205696/8bVQrFvB_400x400.jpg" />
+              <div className="middle-details">
+                <h3>Dalmas Ogembo</h3>
+                <p>@dalmasonto</p>
+              </div>
+              <IconButton className="last-icon">
+                <FaEllipsisH />
+              </IconButton>
+            </div>
+          </Popover>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/pages/notifications/Notifications.js
@@ -0,0 +1,245 @@
+import React, { forwardRef } from 'react'
+import { Avatar, IconButton, Button } from '@material-ui/core';
+import { Popover, Tabs } from 'uiw';
+import { IoEllipsisHorizontal } from 'react-icons/io5';
+import { IoIosCloseCircle, IoIosCog, IoIosSearch } from 'react-icons/io';
+import './../../styles/notifications/Notifications.css';
+
+const users = [
+  {
+    name: 'Dalmas Ogembo',
+    handle: '@alpha',
+    followers: '2000',
+    following: '1300',
+    quote: 'Some quote',
+    followed_by: 'Somebody Omabere'
+  },
+  {
+    name: 'Quality Works',
+    handle: '@quality',
+    followers: '2000',
+    following: '1300',
+    quote: 'Some quote',
+    followed_by: 'Somebody Omabere'
+  },
+  {
+    name: 'Shamaldas Rancho',
+    handle: '@shamaldas',
+    followers: '2000',
+    following: '1300',
+    quote: 'Some quote',
+    followed_by: 'Somebody Omabere'
+  },
+  {
+    name: 'Raider Dante',
+    handle: '@raider',
+    followers: '2000',
+    following: '1300',
+    quote: 'Some quote',
+    followed_by: 'Somebody Omabere'
+  },
+]
+
+function Notifications() {
+  return (
+    <div className="notifications">
+
+      <div className="notifications-left">
+        <div className="notifications-header">
+          <div className="notifications-header-top">
+            <h2>Notifications</h2>
+            <IconButton className="notifications-settings-btn" title="settings">
+              <IoIosCog size={26} />
+            </IconButton>
+          </div>
+        </div>
+        <div className="notifications-body">
+          <div className="notifications-header-tabs">
+            <Tabs type="line" activeKey="1">
+
+              <Tabs.Pane label="All" key="1">
+                <h3>All</h3>
+              </Tabs.Pane>
+              <Tabs.Pane label="Mentions" key="2">
+                <h3>Mentions</h3>
+              </Tabs.Pane>
+
+            </Tabs>
+          </div>
+        </div>
+      </div>
+
+      <div className="notifications-right">
+        <RightSideBar />
+      </div>
+
+    </div>
+  )
+}
+
+const RightSideBar = () => {
+  return (
+    <div className="right-sidebar-component">
+      <div className="right-sidebar-component-header">
+        <div className="twitter-search-component">
+
+          {/* <Popover trigger="click" placement="bottom" content={<TwitterSearchContent />}> */}
+          <div className="twitter-search-component-wrapper">
+            <IoIosSearch size={26} className="twitter-search-component-search-icon" />
+            <Popover trigger="focus" placement="bottom" content={<TwitterSearchContent />} className="search-results-popover">
+              <input className="twitter-search-component-input" placeholder="Search Twitter" />
+            </Popover>
+            <IoIosCloseCircle size={26} className="twitter-search-component-cancel-icon" />
+          </div>
+          {/* </Popover> */}
+
+        </div>
+      </div>
+      <div className="right-sidebar-component-body">
+
+        <TwitterRightSidebarCommon title="Trends for you" right_icon={true}>
+          <Trend title="Dear IMF" subtitle="Trending in Kenya" tweets="15.7K" />
+          <Trend title="IMF EXECUTIVE BOARD" subtitle="Trending in Kenya" tweets="15.8K" />
+          <Trend title="#RutoVsThePeople" subtitle="Trending in Kenya" tweets="5,618" />
+          <Trend title="#museveni" subtitle="Trending in Kenya" tweets="2,256" />
+        </TwitterRightSidebarCommon>
+
+        <TwitterRightSidebarCommon title="Who to follow" right_icon={false}>
+          {
+            users.map((user, index) => {
+              return (
+                <WhoToFollow user={user} />
+              )
+            })
+          }
+        </TwitterRightSidebarCommon>
+
+      </div>
+    </div>
+  )
+}
+
+const TwitterSearchContent = () => {
+  return (
+    <div className="twitter-search-component-results">
+      <p className="keywords text-center">Try searching for people, topics, or keywords</p>
+    </div>
+  )
+}
+
+
+const TwitterRightSidebarCommon = ({ title, right_icon, children }) => {
+  return (
+    <div className="right-sidebar-component-body-common">
+
+      <div className="right-sidebar-component-body-common-header">
+        <h2>{title}</h2>
+        {
+          right_icon &&
+          <IconButton className="trends-settings">
+            <IoIosCog />
+          </IconButton>
+        }
+      </div>
+      <div className="divider" />
+      <div className="right-sidebar-component-body-common-body">
+        {
+          children
+        }
+      </div>
+
+      <div className="right-sidebar-component-body-common-footer">
+        <p>Show more</p>
+      </div>
+
+    </div>
+  )
+}
+
+const Trend = ({ subtitle, title, tweets }) => {
+  return (
+    <div>
+      <div className="twitter-trend">
+        <div className="trend-data">
+          <p> {subtitle} </p>
+          <h3> {title} </h3>
+          <p> {tweets} Tweets</p>
+        </div>
+        <IconButton className="trend-elipsis">
+          <IoEllipsisHorizontal />
+        </IconButton>
+      </div>
+      <div className="divider" />
+    </div>
+  )
+}
+
+const WhoToFollow = ({ user }, ref) => {
+  return (
+    <div>
+      <div className="who-to-follow">
+        <div>
+          <Popover trigger="click" placement="bottom" content={<WhoToFollowHoverPopover ref={ref} user={user} />} className="who-to-follow-popover-inner">
+            <div className="who-to-follow-start">
+              <Avatar className="who-to-follow-start-avatar capitalize">
+                {user?.name[0]}
+              </Avatar>
+              <div className="who-to-follow-start-details">
+                <h3> {user?.name} </h3>
+                <p> {user?.handle} </p>
+              </div>
+            </div>
+          </Popover>
+        </div>
+        <div className="who-to-follow-end">
+          <Button className="follow-btn">
+            Follow
+          </Button>
+        </div>
+      </div>
+      <div className="divider" />
+    </div>
+  )
+}
+const WhoToFollowHoverPopover = forwardRef(({ user }, ref) => {
+  console.log("USER=>", user)
+  return (
+    <div className="who-to-follow-popover">
+      <div className="clear-fix" />
+      <div className="w-100">
+        <div className="who-to-follow-popover-header">
+          <Avatar className="who-to-follow-popover-header-avatar capitalize">
+            {user?.name[0]}
+          </Avatar>
+          <Button className="who-to-follow-popover-header-follow-btn">
+            Follow
+        </Button>
+        </div>
+        <div className="who-to-follow-popover-details">
+          <h3> {user?.name} </h3>
+          <p> {user?.handle} </p>
+        </div>
+        <div className="who-to-follow-popover-followers-following">
+          <div className="who-to-follow-popover-followers-following-follow">
+            <h3> {user?.following} <span>Following</span></h3>
+          </div>
+          <div className="who-to-follow-popover-followers-following-follow">
+            <h3> {user?.followers} <span>Followers</span> </h3>
+          </div>
+        </div>
+        <div className="who-to-follow-popover-quote">
+          <p> {user?.quote} </p>
+        </div>
+        <div className="who-to-follow-popover-footer">
+          <Avatar className="who-to-follow-popover-footer-avatar capitalize">
+            {user?.followed_by[0]}
+          </Avatar>
+          <p>Followed by {user?.followed_by}</p>
+        </div>
+        <div className="clear-fix" />
+      </div>
+    </div>
+  )
+})
+
+export default Notifications
