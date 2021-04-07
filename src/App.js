d06f49918fcf9b46fbd6b65d53b1b4ea9e7c37d4
dalmasonto <dalmasogembo@gmail.com>
2021-04-07 17:27:04 +0300

    notification page update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Explore from './pages/explore/Explore';
 // import Navbar from './global/components/globals/Navbar';
 import Notifications from './pages/notifications/Notifications';
 import './global/styles/MainLayout.css';
+import Bookmarks from './pages/bookmarks/Bookmarks';
+import Lists from './pages/lists/Lsits';
 
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
             <Route exact path='/' component={Home} />
             <Route exact path='/explore' component={Explore} />
             <Route exact path='/notifications' component={Notifications} />
+            <Route exact path='/bookmarks' component={Bookmarks} />
+            <Route exact path='/lists' component={Lists} />
             <Route render={() => ('PAGE NOT FOUND')} />
           </Switch>
         </div>
